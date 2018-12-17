15866a62912af2fff303a38ee5a9384ef7b333c8
glj <GAOLIANGJIE849@pingan.com.cn>
2018-12-17 17:50:13 +0800

    .

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 用于标注创建的缓存，也可以根据它来建立版本规范
-const CACHE_NAME = "lzwme_cache_v1.0.5";
+const CACHE_NAME = "lzwme_cache_v1.1.5";
 // 列举要默认缓存的静态资源，一般用于离线使用
 const urlsToCache = [
     '/pwa/img/1.png',
